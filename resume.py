1131e84f98a7ced635c94393cfb72eaeea7e6e39
Shivendra Narayan Tripathi <shivendra.tripathi767@gmail.com>
2025-09-27 17:12:56 +0530

    Update

--- resume.py
+++ resume.py
@@ -6,10 +6,14 @@ from pathlib import Path
 model=SentenceTransformer("all-MiniLM-L6-v2")
 
 # Loading JDs
+
+BASE_DIR = Path(__file__).resolve().parent  # folder where resume.py is
+JD_DIR = BASE_DIR / "data" / "jds"
+
 def load_jds(path):
     with open (path,'r')as f:
         return json.load(f)
     
-jd=load_jds("data/jds/jd1.json")
+jd=load_jds("jd1.json")
 print(jd['title'])
 print(jd['requirements'])
